33f5664a44f778ef95821751a9de90f480360182
俊华 蔡 <1032031912@qq.com>
2019-11-19 14:58:10 +0800

    创建Models并且引用到登录窗体

--- StudentManager/FrmUserLogin.cs
+++ StudentManager/FrmUserLogin.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using Models;
 
 namespace StudentManager
 {
@@ -21,7 +21,27 @@ namespace StudentManager
         //登录
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            #region 数据验证-输入账号是否按照格式
+            //检查 账号-密码 是否有输入内容
+            if (this.txtLoginId.Text.Trim().Length==0)
+            {
+                MessageBox.Show("请输入登录的账号!", "提示信息");
+                //调整winfrom控件焦点
+                this.txtLoginId.Focus();
+                return;
+            }
+            if (this.txtLoginPwd.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("请输入登录的密码", "提示信息");
+                //调整winfrom控件焦点
+                this.txtLoginPwd.Focus();
+                return;
+            }
+            #endregion
 
+            #region 封装接受到的数据：登录账号/登录密码
+
+            #endregion
         }
         //关闭
         private void btnClose_Click(object sender, EventArgs e)
